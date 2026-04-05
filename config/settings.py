f274f450e32a3ff7be7567293cc078e9c6e7b862
Vlad Lesiv <lesivvladyslav@gmail.com>
2026-04-05 12:38:07 +0200

    upgraded settings to allow host

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='dev-secret-key')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,volunteering-system.vercel.app').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
